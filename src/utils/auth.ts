10513bfcd809bebfa3d85e4a7dcd0bb4ed9af8dd
Vrezh Fedora <vrezhraiden@gmail.com>
2023-06-01 16:48:24 +0200

    Console log access token only for testing

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,5 +7,8 @@ const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 
 const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
+// Log the Oauth token only for testing purposes
+// const token = await authProvider.getAccessToken();
+// console.log("Oauth token: ", token.accessToken);
 
 export default authProvider;
